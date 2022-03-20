aa2421ab9bd823c9b65f53ee2633fbb919c91d7a
henrimory <henrique.mory@hotmail.com>
2022-03-20 00:58:11 -0300

    Melhorias no testes unitários

--- src/test/java/com/henrique/controlefinancas/model/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/henrique/controlefinancas/model/repository/UsuarioRepositoryTest.java
@@ -6,6 +6,9 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -13,9 +16,11 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import com.henrique.controlefinancas.model.entity.Usuario;
 
 
-@SpringBootTest
+
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 public class UsuarioRepositoryTest {
 	
 	@Autowired	
