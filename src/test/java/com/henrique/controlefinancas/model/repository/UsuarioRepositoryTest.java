4da4e6fec3a64b12050408e783c6c593cc655ef2
henrimory <henrique.mory@hotmail.com>
2022-03-20 01:02:35 -0300

    Refatoração no código

--- src/test/java/com/henrique/controlefinancas/model/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/henrique/controlefinancas/model/repository/UsuarioRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -26,11 +27,14 @@ public class UsuarioRepositoryTest {
 	@Autowired	
 	UsuarioRepository repository;
 	
+	@Autowired
+	TestEntityManager entityManager;
+	
 	@Test
 	public void deveVerificarAExistenciaDeUmEmail() {
 		// cenário		
 		Usuario usuario = Usuario.builder().nome("usuario").email("usuario@email.com").build();	
-		repository.save(usuario);
+		entityManager.persist(usuario);
 		
 		// ação/execução
 		boolean result = repository.existsByEmail("usuario@email.com");
@@ -44,7 +48,7 @@ public class UsuarioRepositoryTest {
 	@Test
 	public void deveRetornarFalsoQuandoNaoHouverUsuarioCadastradoComOEmail() {
 		//cenário
-		repository.deleteAll();
+		//repository.deleteAll();
 		
 		//ação
 		boolean result = repository.existsByEmail("usuario@email.com");
