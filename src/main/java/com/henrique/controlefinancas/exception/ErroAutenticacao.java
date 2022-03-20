104bfb459fe2cf4e3675db9ff063e0343dad76bb
henrimory <henrique.mory@hotmail.com>
2022-03-19 23:57:08 -0300

    Restauração ao ponto funcional

---
+++ src/main/java/com/henrique/controlefinancas/exception/ErroAutenticacao.java
@@ -0,0 +1,9 @@
+package com.henrique.controlefinancas.exception;
+
+public class ErroAutenticacao extends RuntimeException{
+	
+	public ErroAutenticacao(String mensagem) {
+		super(mensagem);
+	}
+
+}
